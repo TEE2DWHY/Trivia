66c9fa6be6c57e3ee212ed9f4442b2ab9d0b15df
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-03-27 23:04:15 +0100

    created the 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,7 +4,7 @@ import error from "../assets/images/404.gif"
 const NotFound = () => {
   return (
     <>
-            <h2 style={{marginTop:"2%"}}>Oops... You followed a <strong>Broken Link</strong></h2>
+            <h2 className="broken">Oops... You followed a <strong>Broken Link</strong></h2>
             <img className="error img-fluid" src={error} alt="404"/>
             <Link to="/"><h4>Back to Home Page</h4></Link>
     </>
