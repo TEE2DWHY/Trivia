862560dbe010df2fe8be08216d11d628c2c2df15
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-05-02 00:11:12 +0100

    created isLoading to load api data

--- src/pages/ScoreBoard.js
+++ src/pages/ScoreBoard.js
@@ -1,11 +1,14 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 //styling
 import "./scoreboard.css";
 //url
 import { update } from "../config/url";
 
 const ScoreBoard = () => {
+  // isLoading
+  const [isLoading, setIsLoading] = useState(true);
+  // set friends name and score
   const [scoreDetails, setScoreDetails] = useState([
     {
       friend: "",
@@ -15,40 +18,53 @@ const ScoreBoard = () => {
   //get creator id
   const id = sessionStorage.getItem("id");
 
-  const getScore = async () => {
-    const res = await axios.get(`${update}/${id}`);
-    // console.log(res.data.user.friends);
-    setScoreDetails(res.data.user.friends);
-  };
-  getScore();
-  console.log(scoreDetails);
+  useEffect(() => {
+    const getScore = async () => {
+      try {
+        const res = await axios.get(`${update}/${id}`);
+        setTimeout(() => {
+          setIsLoading(false);
+          setScoreDetails(res.data.user.friends);
+        }, 3000);
+      } catch (err) {
+        alert("an error occurred");
+      }
+    };
+    getScore();
+  });
   return (
     <>
       <h2 className="scoreboard-header">
         ScoreBoard!!! <i class="fa-solid fa-medal"></i>
       </h2>
-      <div className="friends-container">
-        <div className="row">
-          <p className="col-lg-6">Your Friends</p>
-          <p className="col-lg-6">Score</p>
+      {isLoading ? (
+        <div class="spinner-border text-primary" role="status">
+          <span class="visually-hidden">Loading...</span>
         </div>
-        {scoreDetails.map((data) => (
+      ) : (
+        <div className="friends-container">
           <div className="row">
-            <h3 className="col-md-6 col-sm-6">
-              <i class="fa-regular fa-circle-user"></i>
-              {data.friend}
-            </h3>
-            <h3 className="col-md-6 col-sm-6">
-              {data.score < 5 ? (
-                <i class="fa-solid fa-star-half-stroke"></i>
-              ) : (
-                <i class="fa-solid fa-star"></i>
-              )}
-              {data.score}
-            </h3>
+            <p className="col-lg-6">Your Friends</p>
+            <p className="col-lg-6">Score</p>
           </div>
-        ))}
-      </div>
+          {scoreDetails.map((data) => (
+            <div className="row">
+              <h3 className="col-md-6 col-sm-6">
+                <i class="fa-regular fa-circle-user"></i>
+                {data.friend}
+              </h3>
+              <h3 className="col-md-6 col-sm-6">
+                {data.score < 5 ? (
+                  <i class="fa-solid fa-star-half-stroke"></i>
+                ) : (
+                  <i class="fa-solid fa-star"></i>
+                )}
+                {data.score}
+              </h3>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 };
